a013b1adfa957fd1ec7a921b08297f07f4a871d2
unknown <gogochi@example.com>
2026-06-10 17:47:01 +0800

    fix: static assets base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/vue-sfc/'  // 💡 確保前後都有斜線！
 })
